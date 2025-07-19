adc305d572aecc5dca28ae37385f5522e23031d5
DonDuro <66226068+DonDuro@users.noreply.github.com>
2025-07-18 22:09:31 -0400

    Delete client directory

--- client/src/pages/auth.tsx
+++
@@ -1,9 +0,0 @@
-import { AuthModal } from "@/components/auth-modal";
-
-interface AuthPageProps {
-  onSuccess: () => void;
-}
-
-export default function AuthPage({ onSuccess }: AuthPageProps) {
-  return <AuthModal onSuccess={onSuccess} />;
-}
